2eb1ceb06e603b4cab5830fa16720dba46634441
jhapreis <jhapreis@gmail.com>
2022-11-11 20:07:17 -0300

    including prints to log file

--- sim.cc
+++ sim.cc
@@ -12,41 +12,50 @@
 #include "physics.hh"
 #include "action.hh"
 
-int main(int argc, char** argv)
-{
-    G4UIExecutive* ui = 0;
 
-    #ifdef G4MULTITHREADED
-      G4MTRunManager* runManager = new G4MTRunManager;
-    #else
-      G4RunManager* runManager = new G4RunManager;
-    #endif
+int main(int argc, char** argv){
 
-    runManager->SetUserInitialization(new ScintillatorConstruction());
-    runManager->SetUserInitialization(new MyPhysicsList());
-    runManager->SetUserInitialization(new MyActionInitialization());
+  G4UIExecutive* ui = 0;
 
-    G4VModularPhysicsList* physics = new QGSP_BERT();
-    physics->RegisterPhysics(new G4DecayPhysics());
-    runManager->SetUserInitialization(physics);
+  std::cout << " ========== Defining runManager ========== " << std::endl;
 
-    G4VisManager *visManager = new G4VisExecutive();
-    visManager->Initialize();
+  #ifdef G4MULTITHREADED
+    G4MTRunManager* runManager = new G4MTRunManager;
+  #else
+    G4RunManager* runManager = new G4RunManager;
+  #endif
 
-    G4UImanager *UImanager = G4UImanager::GetUIpointer();
 
-    if(argc == 1)
-    { 
-        ui = new G4UIExecutive(argc, argv);
-        UImanager->ApplyCommand("/control/execute vis.mac");
-        ui->SessionStart();
-    }
-    else
-    {
-        G4String command = "/control/execute ";
-        G4String fileName = argv[1];
-        UImanager->ApplyCommand(command+fileName);
-    }
+  std::cout << " ========== Setting ScintillatorConstruction ========== " << std::endl;
+  runManager->SetUserInitialization(new ScintillatorConstruction());
 
-    return 0;
+  std::cout << " ========== Setting PhysicsList ========== " << std::endl;
+  runManager->SetUserInitialization(new MyPhysicsList());
+
+  std::cout << " ========== Setting MyActionInitialization ========== " << std::endl;
+  runManager->SetUserInitialization(new MyActionInitialization());
+
+  std::cout << " ========== Done ========== " << std::endl;
+
+
+
+  G4VisManager *visManager = new G4VisExecutive();
+  visManager->Initialize();
+
+  G4UImanager *UImanager = G4UImanager::GetUIpointer();
+
+  if(argc == 1){ // no macro files passed as arguments
+    std::cout << " ========== Defining UI ========== " << std::endl;
+
+    ui = new G4UIExecutive(argc, argv);
+    UImanager->ApplyCommand("/control/execute vis.mac");
+    ui->SessionStart();
+  }
+  else{
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+
+  return 0;
 }
